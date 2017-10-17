060b06e42a302b020f020c74e7f43c0041d44923
Darren Kwong <kwongdarren@gmail.com>
2017-10-16 18:35:44 -0700

    starter files added

---
+++ server.js
@@ -0,0 +1,28 @@
+//Dependancies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require ("path");
+
+//Setup the express app
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//Survey questions
+var questions = [{
+
+}];
+
+//routes
+app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "survey.html"));
+});
