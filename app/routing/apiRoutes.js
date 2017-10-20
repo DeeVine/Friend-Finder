e74e5c24d1ab081264a855d2766e938c9d7de435
Darren Kwong <kwongdarren@gmail.com>
2017-10-20 00:21:12 -0700

    added reset to default friend list

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -99,9 +99,33 @@ module.exports = function(app) {
 
   app.post("/api/clear", function() {
     // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
+    friends = [
+      {
+        name: "friend1",
+        photoUrl: "http://www.sepeb.com/random-picture/random-picture-013.jpg",
+        scores: [1,5,3,1,2,3,4,2,3,1]
+      },
+      {
+        name: "friend2",
+        photoUrl: "http://www.sepeb.com/random-picture/random-picture-014.jpg",
+        scores: [2,3,5,5,5,3,1,1,5,5]
+      },
+      {
+        name: "friend3",
+        photoUrl: "http://www.sepeb.com/random-picture/random-picture-015.jpg",
+        scores: [2,2,2,2,2,2,3,1,3,3]
+      },
+      {
+        name: "friend4",
+        photoUrl: "http://www.sepeb.com/random-picture/random-picture-016.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+      },
+      {
+        name: "friend5",
+        photoUrl: "http://www.sepeb.com/random-picture/random-picture-017.jpg",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+      }
+    ];
 
-    console.log(tableData);
   });
 };
